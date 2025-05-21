8bed4e88ed8bc6b40bdc37ccf89c0e170f02c945
Adrian Lee <alee.lcagency@gmail.com>
2025-05-21 23:25:14 +0800

    feat: init project (#1)
    Co-authored-by: Adrian Lee <adrian.lee@Adrians-MacBook-Pro-16-M1.local>

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  root: 'app' // Set the root directory to the 'app' folder
+})
