769e19e2e33302234f16f24402657e33fe939fae
Adrian Lee <alee.lcagency@gmail.com>
2025-05-22 15:31:34 +0800

    feat: add attendance api in server (#2)
    Co-authored-by: Adrian Lee <lee.adrian@moneyforward.co.jp>

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import attendanceRouter from './routers/attendance'
 
 const app = new Hono()
 
-app.get('/healthz', c => c.text('OK'))
+const port = 3000
 
-serve(app)
+app.get('/healthz', c => c.text('OK'))
+app.route('/api/attendances', attendanceRouter)
+
+serve(app, () => {
+  console.log(`Server is running on port ${port}`)
+})
