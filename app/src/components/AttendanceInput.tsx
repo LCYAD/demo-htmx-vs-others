b374c855111020528c9c26dddf989f4e58500231
Adrian Lee <lee.adrian@moneyforward.co.jp>
2025-05-23 09:52:54 +0900

    refactor: react app layout

--- app/src/components/AttendanceInput.tsx
+++ app/src/components/AttendanceInput.tsx
@@ -19,28 +19,43 @@ export function AttendanceInput({
   }
 
   return (
-    <div>
-      <Form onSubmit={handleSubmit}>
-        <InputGroup className="mb-3">
-          <Form.Control
-            type="text"
-            placeholder="Name"
-            value={name}
-            onChange={e => setName(e.target.value)}
-            required
-          />
-          <Form.Control
-            type="number"
-            placeholder="Age"
-            value={age}
-            onChange={e => setAge(e.target.value)}
-            required
-          />
-          <Button type="submit" variant="primary">
-            Add
-          </Button>
-        </InputGroup>
-      </Form>
-    </div>
+    <>
+      <h2 className="text-center mb-4">Who is going to attend?</h2>
+      <div
+        style={{
+          marginTop: '20px',
+          height: '100px',
+          border: '2px solid #dee2e6',
+          borderRadius: '10px',
+          padding: '20px'
+        }}
+      >
+        <div style={{ padding: '20px' }}>
+          <Form onSubmit={handleSubmit}>
+            <InputGroup className="mb-3">
+              <Form.Control
+                type="text"
+                placeholder="Name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required
+                style={{ width: '30%', margin: '0px 10px', height: '30px' }}
+              />
+              <Form.Control
+                type="number"
+                placeholder="Age"
+                value={age}
+                onChange={e => setAge(e.target.value)}
+                required
+                style={{ width: '30%', margin: '0px 10px', height: '30px' }}
+              />
+              <Button type="submit" variant="primary">
+                Add
+              </Button>
+            </InputGroup>
+          </Form>
+        </div>
+      </div>
+    </>
   )
 }
